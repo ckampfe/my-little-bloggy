6aa2497137eb99f207ea9bb6a9ba483574ca8ae2
Clark Kampfe <clark.kampfe@gmail.com>
2013-10-31 15:57:40 -0500

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :posts_tags
   has_many :tags, :through => :posts_tags
+
+  validates :body, :presence => true
+  validates :title, :presence => true
 end
