0a3717a863e3f9c981dfc8ebd719e71af536a4c5
Clark Kampfe <clark.kampfe@gmail.com>
2013-10-31 21:27:50 -0500

    unique checking on tag seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 tags = []
 10.times { tags << Tag.create(:name => Faker::Lorem.word) }
 
-3.times do 
+3.times do
   p = Post.create(:title => Faker::Lorem.word,
                :body => Faker::Lorem.paragraphs(3))
-  
-  number_of_tags = rand(tags.length)
-  number_of_tags.times { p.tags << tags.sample }
 
+  number_of_tags = rand(tags.length)
+
+  number_of_tags.times do
+    random_tag = tags.sample
+    p.tags << random_tag unless p.tags.include? random_tag
+  end
 end
